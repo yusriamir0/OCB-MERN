c576a23993a78eddb2becbc69a79b94390df1dc9
Yusri Amir <yusriamir0@gmail.com>
2024-03-12 17:10:55 +0800

    update new lesson

--- Lesson node.js/nodejs-module/fs - file system/fs.js
+++ Lesson node.js/nodejs-module/fs - file system/fs.js
@@ -1,13 +1,37 @@
-const { log } = require("console");
-const fs = require("fs");
+// const fs = require("fs");
 // Create file
 
-fs.writeFile("index.html", "hello world", function (err) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("File created");
-    }
-});
+// Lesson #1
+// fs.writeFile("index.html", "hello world", function (err) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("File created");
+//     }
+// });
 
-fs.writeFile
+// Lesson #2 - Create file using async-await
+// const fs = require("fs/promises");
+
+// const buatFail = async () => {
+//     try {
+//         await fs.writeFile("login.html", "Welcome to login page");
+//         console.log("File created");
+//     } catch (error) {
+//         console.log("Please try again");
+//     }
+// };
+// buatFail();
+
+// Lesson #2.1 - invoke parameter
+const fs = require("fs/promises");
+
+const buatFail = async (createFile, contentSentence) => {
+    try {
+        await fs.writeFile(createFile, contentSentence);
+        console.log("File created");
+    } catch (error) {
+        console.log("Please try again");
+    }
+};
+buatFail("file.pdf", "This sentence is created out from node terminal");
