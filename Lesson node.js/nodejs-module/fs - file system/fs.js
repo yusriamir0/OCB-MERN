cb0c8709c4409beaec478a0265867c19d6f5cd4b
Yusri Amir <yusriamir0@gmail.com>
2024-03-13 14:51:18 +0800

    add new lesson

--- Lesson node.js/nodejs-module/fs - file system/fs.js
+++ Lesson node.js/nodejs-module/fs - file system/fs.js
@@ -1,7 +1,6 @@
+// Lesson #1
 // const fs = require("fs");
 // Create file
-
-// Lesson #1
 // fs.writeFile("index.html", "hello world", function (err) {
 //     if (err) {
 //         console.log(err);
@@ -12,7 +11,6 @@
 
 // Lesson #2 - Create file using async-await
 // const fs = require("fs/promises");
-
 // const buatFail = async () => {
 //     try {
 //         await fs.writeFile("login.html", "Welcome to login page");
@@ -24,14 +22,29 @@
 // buatFail();
 
 // Lesson #2.1 - invoke parameter
-const fs = require("fs/promises");
+// const fs = require("fs/promises");
 
-const buatFail = async (createFile, contentSentence) => {
-    try {
-        await fs.writeFile(createFile, contentSentence);
-        console.log("File created");
-    } catch (error) {
-        console.log("Please try again");
+// const buatFail = async (createFile, contentSentence) => {
+//     try {
+//         await fs.writeFile(createFile, contentSentence);
+//         console.log("File created");
+//     } catch (error) {
+//         console.log("Please try again");
+//     }
+// };
+// buatFail("file.pdf", "This sentence is created out from node terminal");
+
+// Lesson #3 - Read file using readFileSync method
+// const fs = require("fs");
+// Read from a file
+// const readFile = fs.readFileSync("file.pdf");
+// console.log(readFile.toString());
+
+// Lesson #3.1 - Read file using readFile method
+const fs = require("fs");
+const readFile = fs.readFile("file.pdf", function (salah, betul) {
+    if (salah) {
+        console.log(salah);
     }
-};
-buatFail("file.pdf", "This sentence is created out from node terminal");
+    console.log(betul.toString());
+});
