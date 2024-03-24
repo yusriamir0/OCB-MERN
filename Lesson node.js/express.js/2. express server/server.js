9f1b178d4e445c45d87c5468ab54b3cb86e7a323
Yusri Amir <yusriamir0@gmail.com>
2024-03-24 23:32:14 +0800

    add new lesson

---
+++ Lesson node.js/express.js/2. express server/server.js
@@ -0,0 +1,9 @@
+const express = require("express"); // express is similar to a function
+
+const app = express();
+
+app.listen(8080, function () {
+    console.log("Server is running at port 8080");
+});
+
+// https://localhost:8080
