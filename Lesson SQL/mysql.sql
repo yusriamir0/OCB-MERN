d3a69d7788f1dcab8837f6e56a3a224d4c93d483
Yusri Amir <yusriamir0@gmail.com>
2024-04-01 08:37:04 +0800

    add new lesson

--- Lesson SQL/mysql.sql
+++ Lesson SQL/mysql.sql
@@ -1,12 +1,12 @@
--- ! SQLZoo
+-- ! SQLZoo - SELECT Basics
 -- 1) SELECT column_name FROM table_name
--- WHERE column_name = 'item_name'
+-- WHERE column_name = 'string_item'
 -- 2) SELECT column_name1, column_name2 FROM table_name
--- WHERE column_name IN(column_name1, column_name2)
+-- WHERE column_name1 IN(column_name1, column_name2)
 -- 3) SELECT column_name1, column_name2 FROM table_name WHERE column_name2 BETWEEN X AND Y
 -- 4)  SELECT column_name1, column_name2
 --       FROM table_name
---       WHERE column_name1 LIKE "inquiry_string%"
+--       WHERE column_name1 LIKE "inquiry_string%" atau "%inquiry_string"
 -- 5) SELECT column_name1, length(column_name1)
 -- FROM table_name
 -- WHERE length(table_name) > x_number
@@ -14,7 +14,22 @@
 -- 7) SELECT column_name1, column_name2
 -- FROM table_name
 -- WHERE LENGTH(column_name1) = LENGTH(column_name2)
-
+-- 8) SELECT column_name1, column_name2
+-- FROM table_name
+-- WHERE LEFT(column_name1, start_of_string_left) = LEFT(column_name2,start_of_string_left) AND column_name1 <> atau != column_name2
+-- 9) SELECT column_name, FROM table_name WHERE column_name NOT LIKE '%%'
+-- 10) SELECT column_name
+-- FROM table_name
+-- WHERE column_name NOT IN ('item_name1', 'item_name2')
+-- 11) SELECT column_name1, column_name2
+-- FROM table_name
+-- ORDER BY column_name1 DESC, column_name2
+-- 12) SELECT COUNT(column_name)
+-- FROM table_name
+-- WHERE condition;
+-- 13) SELECT DISTINCT column1, column2, ...
+-- FROM table_name;
+--
 -- ! TIL
 --
 -- SHOW DATABASE
