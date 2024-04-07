895fb51b4794051c539babc93fb46180318dd36f
Yusri Amir <yusriamir0@gmail.com>
2024-04-07 16:23:34 +0800

    add new lesson

---
+++ Lesson node.js/Lesson Zahin/lesson.js
@@ -0,0 +1,17 @@
+// Zahin class
+
+// ! Day 1
+
+const variable = require("nodejs-built-in modules");
+
+// ! Day 2
+// Basic node command
+// - node -v
+// - node
+// - node index.js
+// - npm init
+// - npm install ‘package’
+// - npm run ‘script’- npx ‘package’
+
+var fs = require("fs"); //? For  CRUD
+
