5e29535a56ffe134ac26019a848de411ddcbfe1b
Yusri Amir <yusriamir0@gmail.com>
2024-04-08 18:24:12 +0800

    add new lesson

--- Lesson node.js/Lesson Zahin/lesson.js
+++ Lesson node.js/Lesson Zahin/lesson.js
@@ -1,4 +1,21 @@
-// Zahin class
+// * Zahin class
+
+// Model View Controller (MVC) and Database design - 4 sessions
+//  ● Introduction to Node.js and MVC
+//  ● Building Controllers and Routing
+//  ● Views and Templates
+
+// Database & SQL - 2 sessions
+//  ● Understanding database
+//  ● SQL query
+//  ● Installing MySQL
+//  ● Connectiong MySQL with Node JS
+
+// Rest API - 4 sessions
+//  ● Introduction and Setting Up REST API
+//  ● Middleware, Validation, and Authentication
+//  ● Handling Data and File Uploads
+//  ● Handling deployment
 
 // ! Day 1
 
@@ -6,12 +23,31 @@ const variable = require("nodejs-built-in modules");
 
 // ! Day 2
 // Basic node command
-// - node -v
-// - node
-// - node index.js
-// - npm init
-// - npm install ‘package’
-// - npm run ‘script’- npx ‘package’
+// > node -v
+// > node
+// > node index.js
+// > npm init
+// > npm install ‘package’
+// > npm run ‘script’- npx ‘package’
 
-var fs = require("fs"); //? For  CRUD
+//? In built module such as fs, path,...
+// example fs = file system
+var fs = require("fs"); // * For  CRUD
+const kontenTulis = "Konten yang kita nak tulis";
+fs.writeFile("example.txt", kontenTulis, (err) => {
+    if (err) {
+        console.error("Error yang muncul");
+    } else {
+        console.log("file sudah tulis");
+    }
+});
+// contoh lain fs CRUD
+fs.readFile("example.txt", (err) => {});
 
+// OTHER in-built example path
+const path = require("path");
+const { log } = require("console");
+const tempatFail = path.jon(__dirname, "fail", "contoh.txt");
+console.log("nama fail", path.basename(tempatFail));
+
+//! Day 3
